f0aa572178e7964aea04f4c32cb3d6c8183ac872
onkar-vibhute <144338073+kd1-80112@users.noreply.github.com>
2024-02-10 16:55:29 +0530

    Add files via upload
    Generate OTP,Rsset Password

---
+++ Employee_Task_Managment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Logger errorLogger = LoggerFactory.GetErrorLogger("file");
+            errorLogger.LoggerEntry(new Exception("File error"));
+
+            errorLogger = LoggerFactory.GetErrorLogger("console");
+            errorLogger.LoggerEntry(new Exception("Console occurred."));
+
+            errorLogger = LoggerFactory.GetErrorLogger("db");
+            errorLogger.LoggerEntry(new Exception("DataBase error ."));
+        }
+    }
+
+}
